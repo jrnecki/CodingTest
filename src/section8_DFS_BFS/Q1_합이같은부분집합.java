3310bb29ca32f75df0995bfd26dec8947ea2aafe
jrnecki <129943670+jrnecki@users.noreply.github.com>
2023-10-24 09:34:02 +0900

    DFS

---
+++ src/section8_DFS_BFS/Q1_합이같은부분집합.java
@@ -0,0 +1,40 @@
+package section8_DFS_BFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q1_합이같은부분집합 {
+    static String answer="NO";
+    static int n, total=0;
+    static boolean flag=false;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int [] numbers = new int[N];
+        int total = 0;
+        for(int i=0; i<N; i++){
+            numbers[i] = Integer.parseInt(st.nextToken());
+            total+=numbers[i];
+        }
+        DFS(0,0,numbers);
+        System.out.println(answer);
+
+    }
+    public static void DFS(int L, int sum, int[] arr){
+        if(flag) return;
+        if(sum>total/2) return;
+        if(L==n){
+            if(total/2==sum){
+                answer="YES";
+                flag=true;
+            }
+        }
+        else{
+            DFS(L+1, sum+arr[L], arr);
+            DFS(L+1, sum, arr);
+        }
+    }
+}
