138130d2fa65bfbc538684d56a393698feb77ace
jrnecki <129943670+jrnecki@users.noreply.github.com>
2023-11-06 17:44:19 +0900

    dynamic programming

---
+++ src/section10_DynamicProgramming/Q1_계단오르기.java
@@ -0,0 +1,23 @@
+package section10_DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q1_계단오르기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[]dp = new int[n+1];
+
+        // 직관적으로 알 수 있음
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i=3; i<=n; i++){
+            dp[i] = dp[i-2]+dp[i-1];
+        }
+        System.out.println(dp[n]);
+
+    }
+}
---
+++ src/section10_DynamicProgramming/Q3_최대부분증가수열.java
@@ -0,0 +1,34 @@
+package section10_DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q3_최대부분증가수열 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int  n = Integer.parseInt(br.readLine());
+        int [] arr = new int[n];
+        int [] dp = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i =0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int answer = 0;
+        dp[0] = 1;
+        for(int i =1; i<n; i++){
+            int max = 0;
+            for(int j =0; j<i; j++){
+                if(arr[j] <arr[i] & dp[j] > max){
+                    max = dp[j];
+                }
+            }
+            dp[i] = max+1;
+            answer = Math.max(answer, dp[i]);
+        }
+
+        System.out.println(answer);
+
+    }
+}
