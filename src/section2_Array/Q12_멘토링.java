c062c94c3790c899626d8d27b5f30783308c62ec
jrnecki <129943670+jrnecki@users.noreply.github.com>
2023-11-17 11:49:22 +0900

    array

--- src/section2_Array/Q12_멘토링.java
+++ src/section2_Array/Q12_멘토링.java
@@ -7,35 +7,39 @@ import java.util.StringTokenizer;
 
 public class Q12_멘토링 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int [][] students = new int[m][n];
-        for(int i=0; i<m; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j =0; j<n; j++){
-                students[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        int answer =0;
-        for(int i=0; i<m; i++){
-            for(int j =0; j<n; j++){
-                int cnt = 0;
-                for(int k =0; k<m; k++){
-                    int pi= 0;
-                    int pj = 0;
-                    for(int s=0; s<n; s++){
-                        if(students[k][s]==i) pi=s;
-                        if(students[k][s]==j) pj=s;
-                    }
-                    if(pi<pj) cnt++;
-                }
-                if(cnt == m){
-                    answer++;
-                }
-            }
-        }
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int n = Integer.parseInt(st.nextToken());
+      int m = Integer.parseInt(st.nextToken());
+      int [][] arr = new int[m][n];
+      for(int i=0; i<m; i++){
+          st = new StringTokenizer(br.readLine());
+          for(int j=0; j<n; j++){
+              arr[i][j] = Integer.parseInt(st.nextToken());
+          }
+      }
+      int answer = 0;
+      for(int i=1; i<=n; i++){
+          for(int j =1; j<=n; j++){
+              int cnt=0;
+              for(int k=0; k<m; k++){ // 테스트 수
+                  int pi=0;
+                  int pj=0;
+                  for(int s=0; s<n; s++){ // 순위
+                      if(arr[k][s] ==i){
+                          pi = s;
+                      }
+                      if(arr[k][s] ==j){
+                          pj = s;
+                      }
+                  }
+                  if(pi < pj) cnt++;
+              }
+              if(cnt == m){
+                  answer++;
+              }
+          }
+      }
         System.out.println(answer);
     }
 }
